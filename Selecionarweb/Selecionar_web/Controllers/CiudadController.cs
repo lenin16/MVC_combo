b76ce639f78f6f878cc2b513f25fb20ddc46f782
programador <contacto@gmail.com>
2020-05-28 23:10:11 -0400

    combo completo

---
+++ Selecionarweb/Selecionar_web/Controllers/CiudadController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Selecionar_web.Controllers
+{
+    public class CiudadController : Controller
+    {
+        // GET: Ciudad
+        public ActionResult Inicio()
+        {
+            List<SelectListItem> lst = new List<SelectListItem>();
+            using (Models.BD_AlumnoEntities db = new Models.BD_AlumnoEntities())
+            {
+                lst = (from c in db.Ciudad
+                       select new SelectListItem
+                       {
+                           Value = c.Id.ToString(),
+                           Text = c.Nombre
+                       }).ToList();
+            }
+                return View(lst);
+        }
+
+
+        [HttpGet]
+        public JsonResult Alumno(int idCiudad)
+        {
+            List<ElementJson> lst = new List<ElementJson>();
+            using (Models.BD_AlumnoEntities db = new Models.BD_AlumnoEntities())
+            {
+                lst = (from a in db.Alumno
+                       where a.CodCiudad == idCiudad
+                       select new ElementJson
+                       {
+                           Value = a.Id,
+                           Text = a.Nombres
+                       }).ToList();
+            }
+
+            return Json(lst,JsonRequestBehavior.AllowGet);
+        }
+
+        public class ElementJson {
+            public int Value { get; set; }
+            public string Text { get; set; }
+        }
+    }
+}
---
+++ Selecionarweb/Selecionar_web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Selecionar_web.Startup))]
+namespace Selecionar_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
